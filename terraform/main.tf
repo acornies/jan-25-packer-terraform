681c166a6964617acd578734df5a88d66185200b
acornies <andrew.cornies@hashicorp.com>
2024-01-22 15:01:54 -0500

    add sec groups

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,3 +28,42 @@ resource "aws_route_table_association" "rta_subnet_public_east" {
   subnet_id      = aws_subnet.subnet_public_east.id
   route_table_id = aws_route_table.rtb_public_east.id
 }
+
+# us-east-2 region
+resource "aws_security_group" "ssh_east" {
+  name   = "ssh_22"
+  vpc_id = aws_vpc.vpc_east.id
+
+  # SSH access from the VPC
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "http_east" {
+  name   = "http_80"
+  vpc_id = aws_vpc.vpc_east.id
+
+  # SSH access from the VPC
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "allow_egress_east" {
+  name   = "allow_egress"
+  vpc_id = aws_vpc.vpc_east.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
