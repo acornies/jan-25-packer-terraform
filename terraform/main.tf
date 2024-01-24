7cd2b49fbef5296b5b3834137913a6ace5bb23f5
acornies <andrew.cornies@hashicorp.com>
2024-01-24 14:21:45 -0500

    Update condition to check for empty string

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,7 +37,7 @@ resource "aws_instance" "web_servers_frontend" {
   # turn on health check in Terraform Cloud
   lifecycle {
     precondition {
-      condition = data.hcp_packer_image.web_servers.revoke_at != null
+      condition = data.hcp_packer_image.web_servers.revoke_at != null && data.hcp_packer_image.web_servers.revoke_at != ""
       error_message = "Source AMI is revoked."
     }
   }
