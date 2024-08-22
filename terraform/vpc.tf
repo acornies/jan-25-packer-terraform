2db2834c4a3e31eab6f6fbbeaa29edce3573e265
acornies <andrew.cornies@hashicorp.com>
2024-08-22 14:13:37 -0400

    open ssh

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -33,12 +33,12 @@ resource "aws_security_group" "ssh_east" {
   vpc_id = aws_vpc.vpc_east.id
 
   # SSH access from the VPC
-  # ingress {
-  #   from_port   = 22
-  #   to_port     = 22
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_security_group" "http_east" {
