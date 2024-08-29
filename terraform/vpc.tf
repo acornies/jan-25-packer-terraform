0f6c6bd2177c2492b18116c38b098ca1ad277181
acornies <andrew.cornies@hashicorp.com>
2024-08-29 14:28:16 -0400

    fix ssh rules

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -37,8 +37,8 @@ resource "aws_security_group" "ssh_east" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    # cidr_blocks = ["10.1.0.0/24"]
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.1.0.0/24"]
+    # cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
